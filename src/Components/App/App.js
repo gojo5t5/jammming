ec98625df12e85f5bd1cb738c55736c10e1ade64
gojo satoru <hematteo@outlook.com>
2021-07-28 18:01:09 +0200

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component() {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <div className="App-playlist"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
