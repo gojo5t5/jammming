ce2fb96b5d60843beafeb023d04166f1605d9de4
gojo satoru <hematteo@outlook.com>
2021-07-28 18:14:00 +0200

    search results

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import TrackList
+import "./SearchResults.css";
+export class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+      </div>
+    );
+  }
+}
