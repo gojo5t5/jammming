6c8c167df84d114bec2e37a8837b34005f0e2c72
gojo satoru <hematteo@outlook.com>
2021-07-28 18:26:59 +0200

    render Track

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,1 +1,19 @@
 import React from "react";
+import "./Track.css";
+
+export class Track extends React.Component {
+  renderAction() {
+    return <button className="Track-action">isRemoval ? '-' : '+'</button>;
+  }
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3></h3>
+          <p></p>
+        </div>
+        <button className="Track-action"></button>
+      </div>
+    );
+  }
+}
